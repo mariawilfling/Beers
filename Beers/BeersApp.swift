59fd856a07caf2cc8db92ec81e0c7a555c64650a
Maria Wilfling <m.wilfling@web.de>
2024-03-11 11:53:17 +0100

    Initial Commit

---
+++ Beers/BeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeersApp.swift
+//  Beers
+//
+//  Created by Maria Wilfling on 11.03.24.
+//
+
+import SwiftUI
+
+@main
+struct BeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
